5c15db965b68171693db024a58318a7198e979c8
Parth Kabade <151639939+ParthKabade@users.noreply.github.com>
2026-09-26 00:21:55 +0530

    Add files via upload

---
+++ program195.java
@@ -0,0 +1,51 @@
+/*
+iRow=4
+iCol=4
+
+*   *   *   *
+$   $   $   $
+*   *   *   *
+$   $   $   $
+ */
+
+import java.util.*;
+
+class Program187
+{
+    public static void Display(int iRow,int iCol)
+    {
+       int i=0,j=0;
+
+       for(i=1;i<=iRow;i++)
+       {
+            for(j=1;j<=iCol;j++)
+            {
+                if(i%2==0)
+                {
+                    System.out.print("$\t");
+                }
+                else
+                {
+                    System.out.print("*\t");
+                }
+            }
+            System.out.println();
+       }
+       
+    }
+
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue = 0,iValue2=0;
+
+        System.out.println("Enter the number of Row :");
+        iValue = sobj.nextInt();
+
+        System.out.println("Enter the number of Coloum :");
+        iValue2 = sobj.nextInt();
+
+        Display(iValue,iValue2);
+    }
+}
